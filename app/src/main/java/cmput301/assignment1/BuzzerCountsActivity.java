de0a4c1e4320ad9b1197910663996eaa3e49fc7e
Justin Wong <JustinWong@Justins-MacBook-Pro.local>
2015-10-02 22:35:29 -0600

    saving dead but fixed repo

---
+++ app/src/main/java/cmput301/assignment1/BuzzerCountsActivity.java
@@ -0,0 +1,16 @@
+package cmput301.assignment1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JustinWong on 15-10-02.
+ */
+public class BuzzerCountsActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buzzercounts);
+    }
+
+}
